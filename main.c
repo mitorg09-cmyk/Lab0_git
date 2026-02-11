b900dfe949a7931aad40cf9eed92a1df7971a2b9
Mitrofan <mityirg09@mail.ru>
2026-02-11 13:35:11 +0700

    Добавлено тестирование
    Добавлены 5 отладочных массивов с однотипным названием и директива define заменяющая в коде программы стандартное название массива на название тестовых массивов

--- main.c
+++ main.c
@@ -7,10 +7,17 @@
 
 #include <stdio.h>
 #define szz 10
+#define array array5
 
 int main()
 {
-    int array[szz] = {5,5,7,3,3,2,2,1,1,10};
+    int array0[szz] = {5,5,7,3,3,2,2,1,1,10}; // 6
+    int array1[szz] = {5,4,7,4,3,4,2,4,1,4}; // 6
+    int array2[szz] = {1,1,1,1,1,1,1,1,1,1}; // 1
+    int array3[szz] = {0,1,2,3,4,5,6,7,8,9}; // 10
+    int array4[szz] = {1,2,2,2,2,2,2,9,50,2}; // 4
+    int array5[szz] = {-5,5,-7,3,-3,2,-2,1,-1,10}; // 10
+
     int max = array[0];
     for(int i=1;i < szz;i++)
         if(array[i] > max) max = array[i];
